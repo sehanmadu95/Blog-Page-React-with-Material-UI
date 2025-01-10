5e9515dcd1366633129a188430f03270dc28965f
sehan <sehanmadushanka0714@gmail.com>
2025-01-10 15:57:54 +0530

    Update spacing in sidebar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -5,40 +5,47 @@ const SideBar = (props) => {
   return (
     <>
       <Grid item xs={12} md={4}>
-        <Paper elevation={0} sx={{ padding: 2 }}>
-          <Typography variant="h6">{props.title}</Typography>
+        {/* Title and Description */}
+        <Paper elevation={0} sx={{ padding: 2, marginBottom: 3 }}>
+          <Typography variant="h6" gutterBottom>
+            {props.title}
+          </Typography>
           <Typography>{props.description}</Typography>
         </Paper>
 
-        <Typography variant="h6" gutterBottom>
-          {" "}
+        {/* Archives Section */}
+        <Typography variant="h6" gutterBottom sx={{ marginTop: 3 }}>
           Archives
         </Typography>
-        <Divider />
+        <Divider sx={{ marginBottom: 2 }} />
         {props.archives.map((archive) => (
           <Link
             display="block"
             variant="body1"
             href={archive.url}
             key={archive.title}
+            sx={{ marginBottom: 1 }}
           >
             {archive.title}
           </Link>
         ))}
 
-        <Typography variant="h6" gutterBottom>
-          {" "}
+        {/* Social Section */}
+        <Typography variant="h6" gutterBottom sx={{ marginTop: 3 }}>
           Social
         </Typography>
-        <Divider />
-
+        <Divider sx={{ marginBottom: 2 }} />
         {props.social.map((network) => (
           <Link display="block" variant="body1" href="#" key={network.title}>
-            <Grid container direction="row" spacing={1} alignItems="center">
+            <Grid
+              container
+              spacing={1}
+              alignItems="center"
+              sx={{ marginBottom: 1 }}
+            >
               <Grid item>
                 <network.icon />
               </Grid>
-
               <Grid item>{network.name}</Grid>
             </Grid>
           </Link>
